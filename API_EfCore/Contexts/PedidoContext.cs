56b9292ffac67170993ffe8fe8ec87a3f563935b
Guilherme <parra.guioliveira@hotmail.com>
2020-09-21 06:45:22 -0300

    Primeira parte finalizada

---
+++ API_EfCore/Contexts/PedidoContext.cs
@@ -0,0 +1,23 @@
+﻿using API_EfCore.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_EfCore.Contexts
+{
+    public class PedidoContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<PedidoItem> PedidoItens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-9K8CGCB\SQLEXPRESS;Initial Catalog=Pedidos;Persist Security Info=True;User ID=sa;Password=sa132");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
